ca676034cc0dafa57b15722d09a65856b2a7e722
luzihao1234567 <86009648@qq.com>
2020-02-01 05:19:03 +0800

    pwa

---
+++ pwa/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'v4'
+self.addEventListener('install',  event=>{
+  console.log('install', event)
+  // event.waitUntil(self.skipWaiting())
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>{
+    cache.addAll([
+      '/',
+      './index.css'
+    ])
+  }))
+})
+self.addEventListener('activate',  event=>{
+  console.log('activate', event)
+  // event.waitUntil(self.clients.claim())
+  event.waitUntil(caches.keys().then(cacheNames => {
+    return Promise.all(cacheNames.map(cacheName=>{
+      if (cacheName != CACHE_NAME) {
+        return caches.delete(cacheName)
+      }
+    }))
+  }))
+})
+self.addEventListener('fetch',  event=>{
+  console.log('fetch', event)
+  event.respondWith(caches.open(CACHE_NAME).then(cache=>{
+    return cache.match(event.request).then(response=>{
+      if(response) {
+        return response
+      }
+      return fetch(event.request).then(response=>{
+        cache.put(event.request, response.clone())
+        return response
+      })
+    })
+  }))
+})
